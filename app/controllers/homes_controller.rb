7eada02af4bd6c4914d6e107db5b05a8c99844ff
NamNV609 <namnv609@gmail.com>
2017-11-18 16:32:38 +0700

    Add capistrano

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,12 @@
 class HomesController < ApplicationController
   def index
+    @price = 28000
+    @vat = ENV["TAX_PERCENTAGE"]
+    @total = @price + get_vat
+  end
+
+  private
+  def get_vat
+    @price.to_f * (@vat.to_f / 100.0)
   end
 end
